f086df8bdb3b5faa2d0920a9be356b7cb6de4c73
lhl <lhl@randomfoo.net>
2025-12-11 15:48:19 +0000

    latest results

---
+++ run-shisa.sh
@@ -0,0 +1,49 @@
+### shisa-v2.1
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1c-lfm2-350m --num-trials=100 --mode=qa  --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1c-lfm2-350m --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-lfm2-1.2b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-lfm2-1.2b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-llama3.2-3b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-llama3.2-3b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v1-7b-v2.1 --num-trials=100 --mode=qa  --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v1-7b-v2.1 --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-qwen3-8b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-qwen3-8b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-unphi4-14b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-unphi4-14b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-llama3.3-70b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2.1-llama3.3-70b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+
+### shisa-v2
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2-qwen2.5-7b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2-qwen2.5-7b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2-llama3.1-8b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2-llama3.1-8b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2-mistral-nemo-12b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2-mistral-nemo-12b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2-unphi4-14b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2-unphi4-14b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2-mistral-small-24b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2-mistral-small-24b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2-qwen2.5-32b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2-qwen2.5-32b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2-llama3.3-70b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2-llama3.3-70b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
+
+python ./run-vllm.py --model=shisa-ai/shisa-v2-llama3.1-405b --num-trials=100 --mode=qa --temperature=0.7 --top-p 0.9
+python ./run-vllm.py --model=shisa-ai/shisa-v2-llama3.1-405b --num-trials=100 --mode=chat --temperature=0.7 --top-p 0.9
